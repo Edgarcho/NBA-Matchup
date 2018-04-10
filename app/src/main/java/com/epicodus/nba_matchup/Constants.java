a8d1c9de3404382c5bea463a78ad18acab6398af
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-10 16:38:33 -0700

    Assign the value in build config to Sports feeds token

--- app/src/main/java/com/epicodus/nba_matchup/Constants.java
+++ app/src/main/java/com/epicodus/nba_matchup/Constants.java
@@ -1,8 +1,10 @@
 package com.epicodus.nba_matchup;
 
+
 /**
  * Created by Edgar on 4/10/2018.
  */
 
 public class Constants {
+    public static final String SPORTSFEEDS_TOKEN = BuildConfig.SPORTSFEEDS_TOKEN;
 }
