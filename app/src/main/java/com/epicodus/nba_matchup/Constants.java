8cdf83d67f0cabea3b35da6e60775a37ca90de94
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-11 15:32:47 -0700

    Add base url to Constants file

--- app/src/main/java/com/epicodus/nba_matchup/Constants.java
+++ app/src/main/java/com/epicodus/nba_matchup/Constants.java
@@ -7,4 +7,5 @@ package com.epicodus.nba_matchup;
 
 public class Constants {
     public static final String SPORTSFEEDS_TOKEN = BuildConfig.SPORTSFEEDS_TOKEN;
+    public static final String CURRENT_ACTIVE_PLAYER_URL = "https://api.mysportsfeeds.com/v1.2/pull/nba/current/active_players.json";
 }
