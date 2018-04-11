fb05b825e6e63b2ac4f6e2439369e1e7b11466b3
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-11 15:45:56 -0700

    Create new OkHttpClient client

--- app/src/main/java/SportsFeedsService.java
+++ app/src/main/java/SportsFeedsService.java
@@ -1,4 +1,8 @@
+import com.epicodus.nba_matchup.Constants;
+
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 
 /**
  * Created by Edgar on 4/11/2018.
@@ -7,6 +11,8 @@ import okhttp3.Callback;
 public class SportsFeedsService {
 
     public static void activePlayers(Callback callback){
-
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.CURRENT_ACTIVE_PLAYER_URL).newBuilder();
+        String url = urlBuilder.build().toString();
     }
 }
