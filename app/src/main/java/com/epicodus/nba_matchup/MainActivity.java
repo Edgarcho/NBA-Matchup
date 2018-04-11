f75dfafb63484fccd0648bdce2ccdafab25b6f3d
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-11 16:24:55 -0700

    Create a new view.onClickListener for main activity

--- app/src/main/java/com/epicodus/nba_matchup/MainActivity.java
+++ app/src/main/java/com/epicodus/nba_matchup/MainActivity.java
@@ -1,15 +1,32 @@
 package com.epicodus.nba_matchup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.playerName) EditText mPlayerName;
+    @BindView(R.id.findPlayers) Button mFindPlayers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindPlayers.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        String playerName = mPlayerName.getText().toString();
+        Intent intent = new Intent(MainActivity.this, ActivePlayer.class);
+        intent.putExtra("player", playerName);
+        StartActivity(intent);
+    }
 }
