8359481e5c91304d63b88d8620cf6df5d2aa0c22
Edgar Hernandez <edgarchortiz@gmail.com>
2018-04-12 13:56:29 -0700

    Add custom adapter and recyclerView to active player activity

--- app/src/main/java/com/epicodus/nba_matchup/ui/ActivePlayerActivity.java
+++ app/src/main/java/com/epicodus/nba_matchup/ui/ActivePlayerActivity.java
@@ -3,7 +3,10 @@ package com.epicodus.nba_matchup.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.epicodus.nba_matchup.adapters.PlayerListAdapter;
 import com.epicodus.nba_matchup.models.Player;
 import com.epicodus.nba_matchup.R;
 import com.epicodus.nba_matchup.services.SportsFeedsService;
@@ -11,17 +14,25 @@ import com.epicodus.nba_matchup.services.SportsFeedsService;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
 public class ActivePlayerActivity extends AppCompatActivity {
     public ArrayList<Player> mPlayers = new ArrayList<>();
+    private PlayerListAdapter mAdapter;
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_player);
+        ButterKnife.bind(this);
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("player");
         getPlayers(name);
@@ -40,6 +51,11 @@ public class ActivePlayerActivity extends AppCompatActivity {
                 ActivePlayerActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        mAdapter = new PlayerListAdapter(getApplicationContext(), mPlayers);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ActivePlayerActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
 
                     }
                 });
